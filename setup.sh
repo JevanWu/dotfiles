0e726ff1ab5a3343609c390ca5d1ecc50ad0e8f9
Jevan Wu <jevanwu@gmail.com>
2025-03-09 21:52:50 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ ln -s ~/dotfiles/idea/.ideavimrc ~/.ideavimrc
 ln -s ~/dotfiles/system/.zshrc ~/.zshrc
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/vim
+ln -s ~/dotfiles/vscode/settings.json ~/Library/Application Support/Code/User/settings.json
 echo 'hello world😆'
